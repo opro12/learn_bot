182fa5a7fa045c2cc84d03a762d0c923cd1c3c94
Oleg Pronin <o.pro12@mail.ru>
2022-09-17 14:17:46 +0300

    create planet in bot

--- bot.py
+++ bot.py
@@ -1,29 +1,71 @@
-#импорт логирования
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+"""
+Домашнее задание №1
 
+Использование библиотек: ephem
+
+* Установите модуль ephem
+* Добавьте в бота команду /planet, которая будет принимать на вход
+  название планеты на английском, например /planet Mars
+* В функции-обработчике команды из update.message.text получите
+  название планеты (подсказка: используйте .split())
+* При помощи условного оператора if и ephem.constellation научите
+  бота отвечать, в каком созвездии сегодня находится планета.
+
+"""
+#импорт логирования
+from asyncio.log import logger
+from datetime import datetime
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import ephem
 import settings
 
 #включаем логирование
-logging.basicConfig(filename='bot.log', format='[%(asctime)s] [%(levelname)s] => %(message)s', level=logging.INFO) #debag, INFO, warning, error
+logging.basicConfig( 
+  format='[%(asctime)s] [%(levelname)s] => %(message)s', 
+  level=logging.INFO
+) 
+logger = logging.getLogger(__name__)
 
 def greet_user(update, context):
-    print('Вызван /start')
+    logger.info('Вызван /start')
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
-    print(update)
+    logger.info(update)
+    
 
 def talk_to_me(update, context):
     text = update.message.text
-    print(text)
+    logger.info(text)
     update.message.reply_text(text)
+
+def name_planet(update, context):
+    text: str = update.message.text
+    entry_planet = text.split()
+    planet_name = entry_planet[1]
+  
+    if planet_name == 'Mars':
+        planet = ephem.Mars
+    elif planet_name.lower() in ('Pluto', 'Плутон'):
+        planet = ephem.Pluto
+    else:
+        update.message.reply_text('Я незнаю такой планеты((')
+        return
+
+    now = datetime.now()       
+    planet_now = ephem.constellation(planet(now))
+
+    update.message.reply_text(f'Planet: {planet_name}, {planet_now}')
+
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True) 
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', name_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+    
     logging.info('Bot started')
     mybot.start_polling()
     mybot.idle()
